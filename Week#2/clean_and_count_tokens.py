9d81abdc00191112b94aa99da82581808f42e9a6
zalkar <zalkar@zalkar-iMac.local>
2019-03-06 23:02:54 -0500

    Python file modefied: sorting functions are done, need to add some more regex

--- Week#2/clean_and_count_tokens.py
+++ Week#2/clean_and_count_tokens.py
@@ -6,9 +6,10 @@
 #
 
 import re
+import operator
 
 file_name = "test_in.xml"
-out_file_name = "test-out.txt"
+out_file_name = "test_out.txt"
 
 frequency = {}
 
@@ -16,17 +17,35 @@ with open(file_name, 'r') as open_text:
     with open(out_file_name, 'w') as open_out_text:
         text = open_text.read().lower()
 
-        # eliminate <tags> before and substitude them with "" empty sting"
-        match_pattern = re.findall(r'\b[a-z]+\b', text) #get the words you need
+        text = re.sub(r'</.+>|<.+">|<\w+>|<.+/>', '', text)
 
-        for word in match_pattern:
-            count = frequency.get(word, 0)
-            frequency[word] = count + 1
+        # \b[a-z]+\b|\b([a-z]+\'[a-z]+)\b|\b([a-z]+\')|(\'[a-z]+)\b -- for apostrophes
 
-        # sort your dictionary by values (decreasing order)
-        # transfer integer to string and print it
+        open_out_text.write(text)
 
-        frequency_list = frequency.keys()
-
-        for words in frequency_list:
-            print(words, frequency[words])
+        # # eliminate <tags> before and substitude them with "" empty sting"
+        # match_pattern = re.findall(r'\b[a-z]+\b', text) #get the words you need
+        #
+        # # iterates through the array and writes # of occurrences in a dictionary
+        # for word in match_pattern:
+        #     if word in frequency:
+        #         frequency[word] += 1
+        #     else:
+        #         frequency[word] = 1
+        #
+        # # sorts in increasing order based on words frequency, doesn't handle alphabetic comparison
+        # sorted_array = sorted(frequency, key=lambda word: frequency[word])
+        #
+        # # complementary cycle to deal with ties and alphabetic comparison
+        # for i in range(len(sorted_array)):
+        #     for j in range(i+1, len(sorted_array)):
+        #         if frequency[sorted_array[i]] == frequency[sorted_array[j]] and sorted_array[i] < sorted_array[j]:
+        #             sorted_array[i], sorted_array[j] = sorted_array[j], sorted_array[i]
+        #
+        # result = ""
+        #
+        # # iterates through a reversed array and collects results
+        # for i in reversed(range(len(sorted_array))):
+        #     result = result + sorted_array[i] + " " + str(frequency[sorted_array[i]]) + "\n"
+        #
+        # open_out_text.write(result)
