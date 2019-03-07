d69722faa1774cc78300eef6d2ec0615da5de698
zalkar@bennington.edu <zalkar@bennington.edu>
2019-03-06 23:18:29 -0500

    check2

--- Week#2/clean_and_count_tokens.py
+++ Week#2/clean_and_count_tokens.py
@@ -23,29 +23,29 @@ with open(file_name, 'r') as open_text:
 
         open_out_text.write(text)
 
-        # eliminate <tags> before and substitude them with "" empty sting"
-        match_pattern = re.findall(r'\b[a-z]+\b', text) #get the words you need
-
-        # iterates through the array and writes # of occurrences in a dictionary
-        for word in match_pattern:
-            if word in frequency:
-                frequency[word] += 1
-            else:
-                frequency[word] = 1
-
-        # sorts in increasing order based on words frequency, doesn't handle alphabetic comparison
-        sorted_array = sorted(frequency, key=lambda word: frequency[word])
-
-        # complementary cycle to deal with ties and alphabetic comparison
-        for i in range(len(sorted_array)):
-            for j in range(i+1, len(sorted_array)):
-                if frequency[sorted_array[i]] == frequency[sorted_array[j]] and sorted_array[i] < sorted_array[j]:
-                    sorted_array[i], sorted_array[j] = sorted_array[j], sorted_array[i]
-
-        result = ""
-
-        # iterates through a reversed array and collects results
-        for i in reversed(range(len(sorted_array))):
-            result = result + sorted_array[i] + " " + str(frequency[sorted_array[i]]) + "\n"
-
-        open_out_text.write(result)
+        # # eliminate <tags> before and substitude them with "" empty sting"
+        # match_pattern = re.findall(r'\b[a-z]+\b', text) #get the words you need
+        #
+        # # iterates through the array and writes # of occurrences in a dictionary
+        # for word in match_pattern:
+        #     if word in frequency:
+        #         frequency[word] += 1
+        #     else:
+        #         frequency[word] = 1
+        #
+        # # sorts in increasing order based on words frequency, doesn't handle alphabetic comparison
+        # sorted_array = sorted(frequency, key=lambda word: frequency[word])
+        #
+        # # complementary cycle to deal with ties and alphabetic comparison
+        # for i in range(len(sorted_array)):
+        #     for j in range(i+1, len(sorted_array)):
+        #         if frequency[sorted_array[i]] == frequency[sorted_array[j]] and sorted_array[i] < sorted_array[j]:
+        #             sorted_array[i], sorted_array[j] = sorted_array[j], sorted_array[i]
+        #
+        # result = ""
+        #
+        # # iterates through a reversed array and collects results
+        # for i in reversed(range(len(sorted_array))):
+        #     result = result + sorted_array[i] + " " + str(frequency[sorted_array[i]]) + "\n"
+        #
+        # open_out_text.write(result)
