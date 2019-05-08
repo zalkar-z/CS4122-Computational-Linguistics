008b2d6897f0d406ef2d2003e9a95875def5c69c
Zalkar <zalkar@bennington.edu>
2019-05-08 19:58:52 -0400

    File reading is done. Vector normalization is in process

--- Project#5/word_analogy.py
+++ Project#5/word_analogy.py
@@ -31,11 +31,38 @@ import os
 # should_normalize = sys.argv[5]
 # similarity_type = sys.argv[6]
 
+# temporary-manual reading values
 vector_file = "smaller_model.txt"
 vector_file_size = 773  # number of words in smaller_model.txt
+input_directory = r'C:\Users\User\Desktop\Bennington College\term2\Computational_Linguistics\MyGitHub\Project#5\GoogleTestSet'
+output_directory = r'C:\Users\User\Desktop\Bennington College\term2\Computational_Linguistics\MyGitHub\Project#5\output'
 
-vectors = []
+vectors = {}  # a global dictionary for vectors
 
+
+def normalize_vectors(words):
+    for word in words:
+        magnitude = magnitude_of_vector(vectors[word])
+        for i in range(len(vectors[word])):
+            vectors[word][i] = vectors[word][i] / magnitude
+
+
+def solve(line):
+    # split line by whitespace
+    line = line.split()
+
+    # an array for paired words
+    first_pair = [line[0], line[1]]
+    second_pair = [line[2], '']
+
+    # handling normalization
+    if should_normalize:
+        normalize_vectors([first_pair[0], first_pair[1], second_pair[0]])
+
+
+
+
+# Step:1 - reading vectors
 with open(vector_file, 'r') as open_file:
     for line in open_file.readlines():
         # splits a string by whitespaces and converts to a list
@@ -44,8 +71,24 @@ with open(vector_file, 'r') as open_file:
         vector_word = temp_list[0]
         # the rest is list of vector
         vector_list = temp_list[1:]
-        # append to a global list of vector dictionaries
-        vectors.append({vector_word: vector_list})
-        break
+        # add to the global dictionary
+        vectors[vector_word] = list(map(float, vector_list))
 
+# Step:2 - read tests
+for filename in os.listdir(input_directory):
+    # skip hidden files
+    if filename.startswith('.'):
+        continue
+    # skip everything NOT .txt
+    if not filename.endswith('.txt'):
+        continue
+    # join directory path with file path to get the whole address
+    input_filepath = os.path.join(input_directory, filename)
+    output_filepath = os.path.join(output_directory, filename)
+
+    # read from file
+    with open(input_filepath, 'r') as input_file:
+        with open(output_filepath, 'w') as output_file:
+            for line in input_file.readlines():
+                output_file.write(solve(line))
 
