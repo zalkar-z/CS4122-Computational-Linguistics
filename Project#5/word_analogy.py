690841115a178f92abded7beb9173b760ed3197b
Zalkar <zalkar@bennington.edu>
2019-05-09 11:48:03 -0400

    Euclidean distance is done

--- Project#5/word_analogy.py
+++ Project#5/word_analogy.py
@@ -2,6 +2,8 @@
 
 import sys
 import os
+import math
+
 #
 # This is an examples of reading files from directory using sys and Python
 #
@@ -37,6 +39,7 @@ vector_file_size = 773  # number of words in smaller_model.txt
 input_directory = r'C:\Users\User\Desktop\Bennington College\term2\Computational_Linguistics\MyGitHub\Project#5\GoogleTestSet'
 output_directory = r'C:\Users\User\Desktop\Bennington College\term2\Computational_Linguistics\MyGitHub\Project#5\output'
 should_normalize = 1
+similarity_type = 0
 
 
 vectors = {}  # a global dictionary for vectors
@@ -65,6 +68,7 @@ def vector_difference(first, second):
 
     return result
 
+
 #
 # Function: calculates the magnitude of a given vector.
 # Return:   A magnitude of a given vector.
@@ -88,6 +92,35 @@ def normalize_vectors(words):
             vectors[word][i] = vectors[word][i] / magnitude
 
 
+def euclidean_distance(first_vector, second_vector):
+    dist = [(a - b) ** 2 for a, b in zip(first_vector, second_vector)]
+    dist = math.sqrt(sum(dist))
+    return dist
+
+
+def best_euclidean_distance(first_vector):
+    best_distance = float('inf')
+    best_word = ''
+
+    for word in vectors:
+        second_vector = vectors[word]
+        current_distance = euclidean_distance(first_vector, second_vector)
+
+        if current_distance < best_distance:
+            best_distance = current_distance
+            best_word = word
+
+    return best_word
+
+
+def best_manhattan_distance(first_vector, second_vector):
+    return ''
+
+
+def best_cosine_distance(first_vector, second_vector):
+    return ''
+
+
 def solve(line):
     # split line by whitespace
     line = line.split()
@@ -101,47 +134,57 @@ def solve(line):
         normalize_vectors([first_pair[0], first_pair[1], second_pair[0]])
 
     # handling vector addition and subtraction
-    summary_vector = vector_sum(second_pair[0], first_pair[1])
-    summary_vector = vector_difference(summary_vector, first_pair[0])
+    summary_vector = vector_sum(vectors[second_pair[0]], vectors[first_pair[1]])
+    summary_vector = vector_difference(summary_vector, vectors[first_pair[0]])
 
     # handling similarity metrics
-    
+    if similarity_type == 0:
+        result = best_euclidean_distance(summary_vector)
+    elif similarity_type == 1:
+        result = best_manhattan_distance(summary_vector)
+    else:
+        result = best_cosine_distance(summary_vector)
 
+    second_pair[1] = result
+    return first_pair[0] + ' ' + first_pair[1] + ' ' + second_pair[0] + ' ' + second_pair[1] + '\n'
 
-
-# # Step:1 - reading vectors
-# with open(vector_file, 'r') as open_file:
-#     for line in open_file.readlines():
-#         # splits a string by whitespaces and converts to a list
-#         temp_list = line.split()
-#         # save the first one as word
-#         vector_word = temp_list[0]
-#         # the rest is list of vector
-#         vector_list = temp_list[1:]
-#         # add to the global dictionary
-#         vectors[vector_word] = list(map(float, vector_list))
-#
-# # Step:2 - read tests
-# for filename in os.listdir(input_directory):
-#     # skip hidden files
-#     if filename.startswith('.'):
-#         continue
-#     # skip everything NOT .txt
-#     if not filename.endswith('.txt'):
-#         continue
-#     # join directory path with file path to get the whole address
-#     input_filepath = os.path.join(input_directory, filename)
-#     output_filepath = os.path.join(output_directory, filename)
-#
-#     # read from file
-#     with open(input_filepath, 'r') as input_file:
-#         with open(output_filepath, 'w') as output_file:
-#             for line in input_file.readlines():
-#                 output_file.write(solve(line))
-#
 
 def main():
+    # Step:1 - reading vectors
+    with open(vector_file, 'r') as open_file:
+        for line in open_file.readlines():
+            # splits a string by whitespaces and converts to a list
+            temp_list = line.split()
+            # save the first one as word
+            vector_word = temp_list[0]
+            # the rest is list of vector
+            vector_list = temp_list[1:]
+            # add to the global dictionary
+            vectors[vector_word] = list(map(float, vector_list))
 
+    samples_amount = 0
+
+    # Step:2 - read tests
+    for filename in os.listdir(input_directory):
+        # skip hidden files
+        if filename.startswith('.'):
+            continue
+        # skip everything NOT .txt
+        if not filename.endswith('.txt'):
+            continue
+        # join directory path with file path to get the whole address
+        input_filepath = os.path.join(input_directory, filename)
+        output_filepath = os.path.join(output_directory, filename)
+
+        # read from file
+        with open(input_filepath, 'r') as input_file:
+            with open(output_filepath, 'w') as output_file:
+                for line in input_file.readlines():
+                    samples_amount += 1
+                    output_file.write(solve(line))
+
+        break
+    print(samples_amount)
 
 
 if __name__ == "__main__":
