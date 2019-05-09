c4ca0fd36887b14d516c06c8c450bff1e01bd3e8
Zalkar <zalkar@bennington.edu>
2019-05-09 12:12:58 -0400

    checker.py is done and tested

--- Project#5/checker.py
+++ Project#5/checker.py
@@ -4,7 +4,22 @@ from itertools import product
 first_file = r'C:\Users\User\Desktop\Bennington College\term2\Computational_Linguistics\MyGitHub\Project#5\GoogleTestSet\capital-common-countries.txt'
 second_file = r'C:\Users\User\Desktop\Bennington College\term2\Computational_Linguistics\MyGitHub\Project#5\output\capital-common-countries.txt'
 
+list1 = []
+list2 = []
+
 with open(first_file, 'r') as first:
-    with open(second_file, 'r') as second:
-        for line1, line2 in product(first.readlines(), second.readlines()):
-            print(line1, '-', line2)
+    for line1 in first.readlines():
+        list1.append(line1)
+
+with open(second_file, 'r') as second:
+    for line2 in second.readlines():
+        list2.append(line2)
+
+count = 0
+
+for i in range(len(list1)):
+    if list1[i] == list2[i]:
+        count += 1
+
+print(count)
+print(count / len(list1) * 100, '%')
