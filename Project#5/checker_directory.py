ce7304ba326d7839b9c850b31a0dafabf9397541
zalkar@bennington.edu <zalkar@bennington.edu>
2019-05-12 22:55:47 -0400

    dictionary checker is done and tested

--- Project#5/checker_directory.py
+++ Project#5/checker_directory.py
@@ -1,28 +1,51 @@
+#!/usr/bin/env python3
+#
+# checker_directory.py - Computational Linguistics Project#5
+# Author: Zak Ziiaidin uulu (zalkar@bennington.edu)
+# Date Created: 05/09/2019
+#
 
-from itertools import product
+import os
+from checker_file import compare_two_files
 
-first_directory = r'/Users/zalkar/Desktop/Computational_Linguistics/Project#5/GoogleTestSet' # mac
-# first_directory = r'C:\Users\User\Desktop\Bennington College\term2\Computational_Linguistics\MyGitHub\Project#5\GoogleTestSet' # windows
+input_directory = r'/Users/zalkar/Desktop/Computational_Linguistics/Project#5/GoogleTestSet' # mac
+# input_directory = r'C:\Users\User\Desktop\Bennington College\term2\Computational_Linguistics\MyGitHub\Project#5\GoogleTestSet' # windows
 
-second_directory = r'/Users/zalkar/Desktop/Computational_Linguistics/Project#5/output' # mac
-# second_directory = r'C:\Users\User\Desktop\Bennington College\term2\Computational_Linguistics\MyGitHub\Project#5\output' # windows
+output_directory = r'/Users/zalkar/Desktop/Computational_Linguistics/Project#5/output' # mac
+# output_directory = r'C:\Users\User\Desktop\Bennington College\term2\Computational_Linguistics\MyGitHub\Project#5\output' # windows
 
-list1 = []
-list2 = []
 
-with open(first_file, 'r') as first:
-    for line1 in first.readlines():
-        list1.append(line1)
+def compare_two_directories(input_directory, output_directory):
+    counter = 0
+    sum = 0
 
-with open(second_file, 'r') as second:
-    for line2 in second.readlines():
-        list2.append(line2)
+    for filename in os.listdir(input_directory):
+        # skip hidden files
+        if filename.startswith('.'):
+            continue
+        # skip everything NOT .txt
+        if not filename.endswith('.txt'):
+            continue
 
-count = 0
+        # join directory path with file path to get the whole address
+        input_filepath = os.path.join(input_directory, filename)
+        output_filepath = os.path.join(output_directory, filename)
 
-for i in range(len(list1)):
-    if list1[i] == list2[i]:
-        count += 1
+        # percentage of similarities between two files
+        percentage = compare_two_files(input_filepath, output_filepath)
 
-print(count)
-print(count / len(list1) * 100, '%')
+        # counting average
+        sum += percentage
+        counter += 1
+
+        print(filename, percentage)
+
+    return sum / counter
+
+
+def main():
+    print(compare_two_directories(input_directory, output_directory))
+
+
+if __name__ == "__main__":
+    main()
