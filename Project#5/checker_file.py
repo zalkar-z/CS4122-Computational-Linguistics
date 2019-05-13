a751ad9d86e650339ef0b1e8be2b7e5e832b521d
zalkar@bennington.edu <zalkar@bennington.edu>
2019-05-12 22:43:28 -0400

    file checker refactored

--- Project#5/checker_file.py
+++ Project#5/checker_file.py
@@ -1,5 +1,9 @@
-
-from itertools import product
+#!/usr/bin/env python3
+#
+# checker_file.py - Computational Linguistics Project#5
+# Author: Zak Ziiaidin uulu (zalkar@bennington.edu)
+# Date Created: 05/09/2019
+#
 
 
 first_file = r'/Users/zalkar/Desktop/Computational_Linguistics/Project#5/GoogleTestSet/currency.txt' # mac
@@ -10,22 +14,31 @@ second_file = r'/Users/zalkar/Desktop/Computational_Linguistics/Project#5/output
 # second_file = r'C:\Users\User\Desktop\Bennington College\term2\Computational_Linguistics\MyGitHub\Project#5\output\currency.txt' # windows
 # second_file = r'/home/zalkar/Computational_Linguistics/Project#5/output/gram6-nationality-adjective.txt' # linux
 
-list1 = []
-list2 = []
 
-with open(first_file, 'r') as first:
-    for line1 in first.readlines():
-        list1.append(line1)
+def compare_two_files(first_file_address, second_file_address):
+    list1 = []
+    list2 = []
 
-with open(second_file, 'r') as second:
-    for line2 in second.readlines():
-        list2.append(line2)
+    with open(first_file_address, 'r') as first:
+        for line1 in first.readlines():
+            list1.append(line1)
 
-count = 0
+    with open(second_file_address, 'r') as second:
+        for line2 in second.readlines():
+            list2.append(line2)
 
-for i in range(len(list1)):
-    if list1[i] == list2[i]:
-        count += 1
+    count = 0
 
-print(count)
-print(count / len(list1) * 100, '%')
+    for i in range(len(list1)):
+        if list1[i] == list2[i]:
+            count += 1
+
+    return count / len(list1) * 100
+
+
+def main():
+    print(compare_two_files(first_file, second_file))
+
+
+if __name__ == "__main__":
+    main()
