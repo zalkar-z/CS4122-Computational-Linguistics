e8a690adad5810670677cb50de8de53d13842a8d
Zalkar <zalkar@bennington.edu>
2019-05-13 20:28:40 -0400

    trash files deleted

--- Project#5/test.py
+++
@@ -1,6 +0,0 @@
-import numpy
-
-v1 = numpy.array([1.0, 1.0, 1.0, 0.0, 1.0], dtype=float)
-v2 = numpy.array([1.0, 0.0, 1.0, 1.0, 1.0], dtype=float)
-
-print(numpy.dot(v1, v2) / (numpy.linalg.norm(v1) * numpy.linalg.norm(v2)))
