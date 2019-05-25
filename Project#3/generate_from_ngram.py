7a9b09ee2a592552bf18dfc9e5986581ca2b41e3
Zalkar <zalkar@bennington.edu>
2019-05-25 16:40:12 -0400

    Part#2 initialized

---
+++ Project#3/generate_from_ngram.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#
+# build_ngram_model.py - Computational Linguistics Project#3
+# Author: Zak Ziiaidin uulu (zalkar@bennington.edu)
+# Date Created: 04/01/2019
+#
+
+import sys
+import math
+import random
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+
+
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
